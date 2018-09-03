bb1aa2df5e78d8f0b52c0ff28625b795d5034d7f
Simon de Bakker <simon@protospace.nl>
2018-09-03 16:45:35 +0200

    Set camera id and change IP address accordingly

---
+++ firmware/update_camera_id.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ID=$1
+IP_BASE=192.168.2
+IP_END=150
+
+usage() {
+	echo "Usage: ${0} [id]"
+}
+
+if [ -z "${ID}" ]; then
+	usage
+	exit 1
+fi
+
+echo ${ID} > /etc/camera_id
+IP_END=$(( $IP_END + $ID ))
+IP=${IP_BASE}.${IP_END}
+
+echo "Setting IP addres to: ${IP}"
+cat "fs/dhcpcd.conf" | sed -e "s/{CAMERA_IP}/${IP}/" > /etc/dhcpcd.conf
+
